9a3c61ff27accce187454bc243d84ac5e5541f01
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2022-04-01 16:57:50 -0500

    format document

--- Analyser.C
+++ Analyser.C
@@ -1,6 +1,6 @@
 #include <ROOT/RNTuple.hxx>
 #include <ROOT/RNTupleModel.hxx>
- 
+
 #include <iostream>
 #include <fstream>
 #include <memory>
@@ -8,20 +8,20 @@
 using RNTupleReader = ROOT::Experimental::RNTupleReader;
 using ENTupleShowFormat = ROOT::Experimental::ENTupleShowFormat;
 
-int main(int argc, char** argv) 
+int main(int argc, char **argv)
 {
-    if(argc!=2)
+    if (argc != 2)
     {
-        std::cout<<"Error! Please specify the location of the output file!"<<std::endl;
-        std::cout<<"Example: ./task2_analyser output_file.root"<<std::endl;
+        std::cout << "Error! Please specify the location of the output file!" << std::endl;
+        std::cout << "Example: ./task2_analyser output_file.root" << std::endl;
         return 0;
     }
 
-    char const* kNTupleFileName = argv[1];
+    char const *kNTupleFileName = argv[1];
 
     auto ntuple = RNTupleReader::Open("Data", kNTupleFileName);
     ntuple->PrintInfo();
-    std::cout<<"The third entry is shown below:"<<std::endl;
+    std::cout << "The third entry is shown below:" << std::endl;
     ntuple->Show(2, ENTupleShowFormat::kCompleteJSON);
     return 0;
 }
